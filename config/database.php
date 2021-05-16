1130a4da06bd5544237ee0f8eb63c9d31e83273d
Russkazkin <ruslan@skazkin.su>
2021-05-16 10:42:52 +0300

    Add schema into db config

--- config/database.php
+++ config/database.php
@@ -69,12 +69,12 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
+            'schema' => env('DB_SCHEMA', 'public'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema' => 'public',
             'sslmode' => 'prefer',
         ],
 
