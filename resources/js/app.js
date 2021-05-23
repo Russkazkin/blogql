232f6325059839eded0b9d288cd840548bdf5fa1
Russkazkin <ruslan@skazkin.su>
2021-05-23 11:29:37 +0300

    Install and setup vue apollo

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,29 @@ import './bootstrap';
 import 'alpinejs';
 import Vue from "vue";
 import VueRouter from 'vue-router';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo'
 
 import PostList from "./components/PostList";
 import Post from "./components/Post";
 
 window.Vue = Vue;
+Vue.use(VueApollo);
 Vue.use(VueRouter);
 
 const routes = [
   {path: '/', name: 'home', component: PostList},
   {path: '/post/:id', name: 'post', component: Post},
 ];
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'http://blogql.skazkin/graphql'
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 const router = new VueRouter({
   mode: "history",
@@ -21,5 +33,6 @@ const router = new VueRouter({
 
 const app = new Vue({
   el: '#app',
+  apolloProvider,
   router,
 });
