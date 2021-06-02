faab21bf069294441186c96f13a1c5d59c66fb3d
Russkazkin <ruslan@skazkin.su>
2021-06-02 11:17:12 +0300

    Update user factory with avatar

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'avatar' => collect(['ahmad.jpg', 'akio.jpg', 'britney.jpg', 'daniel.jpg', 'harry.jpeg', 'john.png', 'jordan.jpg', 'june.jpg', 'marcel.jpg', 'rob.png', 'tim.jpg'])->random(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
--- database/migrations/2021_06_02_065936_add_avatar_column_to_users_table.php
+++ database/migrations/2021_06_02_065936_add_avatar_column_to_users_table.php
@@ -14,7 +14,7 @@ class AddAvatarColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar');
+            $table->string('avatar')->nullable()->after('email');
         });
     }
 
