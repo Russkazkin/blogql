33f1f637d78c2a668956917934cb2e7a9637f4a0
Russkazkin <ruslan@skazkin.su>
2021-06-03 08:49:57 +0300

    Output data in the post page template

--- resources/js/components/Post.vue
+++ resources/js/components/Post.vue
@@ -1,16 +1,17 @@
 <template>
 <div class="container mx-auto px-4 w-full md:w3/4 lg:w3/5 xl:w1/2 mt-20">
-  <div>
-    <div class="text-lg text-gray-600">By Rus Skazkin in Links • 3 hours ago</div>
-    <h1 class="text-5xl mt-10 font-bold mb-12">The post title</h1>
-    <p class="text-gray-700 pb-3 mb-12 whitespace-pre-line">The blog post content</p>
+  <div v-if="$apollo.loading">Loading...</div>
+  <div v-else>
+    <div class="text-lg text-gray-600">By {{ post.author.name }} in {{ post.topic.name }} • 3 hours ago</div>
+    <h1 class="text-5xl mt-10 font-bold mb-12">{{ post.title}}</h1>
+    <p class="text-gray-700 pb-3 mb-12 whitespace-pre-line">{{ post.content }}</p>
     <div class="mb-24 flex">
       <div class="mr-6">
-        <img src="/storage/faces/ahmad.jpg" alt="Author avatar" class="w-16 h-16 rounded-full">
+        <img :src="`/storage/faces/${post.author.avatar}`" alt="Author avatar" class="w-16 h-16 rounded-full">
       </div>
       <div class="flex flex-col justify-center">
-        <div class="text-xl text-gray-600">Written by Rus Skazkin</div>
-        <div class="text-gray-600">Published in Link on May 20, 2021</div>
+        <div class="text-xl text-gray-600">Written by {{ post.author.name }}</div>
+        <div class="text-gray-600">Published in {{ post.topic.name }} on May 20, 2021</div>
       </div>
     </div>
   </div>
