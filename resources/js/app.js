9bf3fc68b063a84035056c2626cb7716f0b4bf91
Russkazkin <ruslan@skazkin.su>
2021-06-04 06:44:02 +0300

    Install prettier

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,34 @@
-import './bootstrap';
-import 'alpinejs';
+import "./bootstrap";
+import "alpinejs";
 import Vue from "vue";
-import VueRouter from 'vue-router';
-import ApolloClient from 'apollo-boost';
-import VueApollo from 'vue-apollo'
+import VueRouter from "vue-router";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
 import PostList from "./components/PostList";
 import Post from "./components/Post";
 import TopicPostList from "./components/TopicPostList";
+import AuthorPostList from "./components/AuthorPostList";
 
 window.Vue = Vue;
 Vue.use(VueApollo);
 Vue.use(VueRouter);
 
 const routes = [
-  {path: '/', name: 'home', component: PostList},
-  {path: '/post/:id', name: 'post', component: Post},
-  {path: '/topics/:slug', name: 'topics', component: TopicPostList},
+  { path: "/", name: "home", component: PostList },
+  { path: "/post/:id", name: "post", component: Post },
+  { path: "/authors/:id", name: "authors", component: AuthorPostList },
+  { path: "/topics/:slug", name: "topics", component: TopicPostList },
 ];
 
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
-  uri: 'http://localhost:3000/graphql',
+  uri: "http://localhost:3000/graphql",
 });
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
-})
+});
 
 const router = new VueRouter({
   mode: "history",
@@ -34,7 +36,7 @@ const router = new VueRouter({
 });
 
 const app = new Vue({
-  el: '#app',
+  el: "#app",
   apolloProvider,
   router,
 });
---
+++ resources/js/components/AuthorPostList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "AuthorPostList"
+}
+</script>
+
+<style scoped>
+
+</style>
